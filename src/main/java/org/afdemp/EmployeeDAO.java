67dc4890b166486cc4be8284f23c844ff3dbd3e3
davidoster <paspa@hotmail.com>
2019-03-07 07:45:54 +0200

    SpringMVCTutorial with JdbcTemplate and Bootstrap

---
+++ src/main/java/org/afdemp/EmployeeDAO.java
@@ -0,0 +1,11 @@
+package org.afdemp;
+
+import java.util.List;
+public interface EmployeeDAO
+{
+    public void saveEmployee(Employee employee);
+    public Employee getEmployeeById(int id);
+    public void updateEmployee(Employee employee);
+    public void deleteEmployee(int id);
+    public List<Employee> getAllEmployees();
+}
